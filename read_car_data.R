883ef909dbf6f1fccf5c5f298e721f9c6a15d165
MAGAÑA MASCUÑANA <mimamas@EVIRL-037-OK.dsic.cloud>
2020-11-27 20:57:13 +0100

    #Done: Spain data extracted

--- read_car_data.R
+++ read_car_data.R
@@ -1,3 +1,3 @@
 carSales <- readxl::read_xlsx("./Datasets/datos_vehiculos_nuevos/total_sales_2019.xlsx", skip = 5,col_names = TRUE)
 carSales = carSales[,!names(carSales) %in% c("...2", "...3","...4")]
-
+spain = na.omit(carSales[carSales[, "REGIONS/COUNTRIES"] == "SPAIN",])
